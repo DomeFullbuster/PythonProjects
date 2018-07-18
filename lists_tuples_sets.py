60ed875a415943ccecae2a344fa4fedad458aae7
Cristian Sosu <cristian.sosu@punct.ro>
2018-07-18 15:51:17 +0300

    Initial commit

---
+++ lists_tuples_sets.py
@@ -0,0 +1,18 @@
+#my_varaible = "hello"
+#grades = [77, 80, 90]
+#tuple_grades = (77, 80, 90) #immutable
+#set_grades = {77, 80, 90} #unique & unordered
+
+#set_grades(60)
+#set_grades(60)
+#print(set_grades)
+
+
+##Set operators
+
+your_lottery_numbers = {1, 2, 3, 4, 5}
+winning_number = {1, 3, 5, 7, 9, 11}
+
+#print(your_lottery_numbers.union(winning_number))
+#print({1, 2, 3, 4}.difference({1, 2}))
+
